c1f12fa96bd7cc111d42afde433aa13f61f122b6
Simon Mourier <simon_mourier@hotmail.com>
2024-02-01 18:36:31 +0100

    Added Ole/Com initialize

--- RegsvrCli/RegsvrCli.cpp
+++ RegsvrCli/RegsvrCli.cpp
@@ -3,6 +3,8 @@
 #include <strsafe.h>
 #include <stdio.h>
 
+typedef HRESULT(WINAPI* DllRegUnRegServer)();
+
 void Help()
 {
 	wprintf(L"Usage is RegsvrCli <path> [/u]\n");
@@ -15,15 +17,31 @@ std::wstring GetErrorMessage(int error)
 	wchar_t message[1024];
 	if (!FormatMessage(FORMAT_MESSAGE_FROM_SYSTEM | FORMAT_MESSAGE_IGNORE_INSERTS | FORMAT_MESSAGE_ARGUMENT_ARRAY, nullptr, error, 0, message, _countof(message), nullptr))
 		return L"";
-	
+
 	return L": " + std::wstring(message);
+}
+
+std::wstring to_wstring(const std::string& s)
+{
+	if (s.empty())
+		return std::wstring();
+
+	auto ssize = (int)s.size();
+	auto wsize = MultiByteToWideChar(CP_THREAD_ACP, 0, s.data(), ssize, nullptr, 0);
+	if (!wsize)
+		return std::wstring();
+
+	std::wstring ws;
+	ws.resize(wsize);
+	wsize = MultiByteToWideChar(CP_THREAD_ACP, 0, s.data(), ssize, &ws[0], wsize);
+	if (!wsize)
+		return std::wstring();
+
+	return ws;
 }
 
 int main()
 {
-	auto reg = true;
-	wchar_t path[2048] = L"";
-
 	SYSTEMTIME st{};
 	GetLocalTime(&st);
 	auto bitness = (sizeof(void*) == 8) ? 64 : 32;
@@ -37,6 +55,8 @@ int main()
 		return -1;
 	}
 
+	auto reg = true;
+	wchar_t path[2048] = L"";
 	for (auto i = 1; i < argc; i++)
 	{
 		if (!_wcsicmp(argv[i], L"/u"))
@@ -54,28 +74,67 @@ int main()
 		return -1;
 	}
 
-	HRESULT hr = S_OK;
-	auto h = LoadLibrary(path);
+	auto coInit = false;
+	auto oleInit = true;
+	auto hr = OleInitialize(nullptr);
+	if (FAILED(hr))
+	{
+		wprintf(L"(warning) Calling OleInitialize() returned error 0x%08X (%i)%s\n", hr, hr, GetErrorMessage(hr).c_str());
+		oleInit = false;
+		hr = CoInitialize(nullptr);
+		if (FAILED(hr))
+		{
+			wprintf(L"(warning) Calling CoInitialize() returned error 0x%08X (%i)%s\n", hr, hr, GetErrorMessage(hr).c_str());
+			hr = S_OK;
+		}
+		else
+		{
+			coInit = true;
+		}
+	}
+
+	auto h = LoadLibraryEx(path, nullptr, LOAD_WITH_ALTERED_SEARCH_PATH);
 	if (!h)
 	{
 		hr = HRESULT_FROM_WIN32(GetLastError());
 		wprintf(L"Calling LoadLibrary(\"%s\") returned error 0x%08X (%i)%s\n", path, hr, hr, GetErrorMessage(hr).c_str());
-		return hr;
-	}
-
-	typedef HRESULT(WINAPI* DllRegUnRegServer)();
-	auto fn = (DllRegUnRegServer)GetProcAddress(h, reg ? "DllRegisterServer" : "DllUnregisterServer");
-	if (!fn)
-	{
-		hr = HRESULT_FROM_WIN32(GetLastError());
-		wprintf(L"Calling GetProcAddress(\"%s\") returned error 0x%08X (%i)%s\n", reg ? L"DllRegisterServer" : L"DllUnregisterServer", hr, hr, GetErrorMessage(hr).c_str());
 	}
 	else
 	{
-		hr = fn();
-		wprintf(L"%s '%s' returned: 0x%08X (%i)\n", reg ? L"Registering" : L"Unregistering", path, hr, hr);
+		char name[64] = "";
+		DllRegUnRegServer fn;
+		if (reg)
+		{
+			StringCchCopyA(name, _countof(name), "DllRegisterServer");
+			fn = (DllRegUnRegServer)GetProcAddress(h, name);
+		}
+		else
+		{
+			StringCchCopyA(name, _countof(name), "DllUnregisterServer");
+			fn = (DllRegUnRegServer)GetProcAddress(h, name);
+		}
+
+		if (!fn)
+		{
+			hr = HRESULT_FROM_WIN32(GetLastError());
+			wprintf(L"Calling GetProcAddress(\"%s\") returned error 0x%08X (%i)%s\n", to_wstring(name).c_str(), hr, hr, GetErrorMessage(hr).c_str());
+		}
+		else
+		{
+			hr = fn();
+			wprintf(L"Calling %s() on '%s' returned: 0x%08X (%i)\n", to_wstring(name).c_str(), path, hr, hr);
+		}
+
+		FreeLibrary(h);
 	}
 
-	FreeLibrary(h);
+	if (coInit)
+	{
+		CoUninitialize();
+	}
+	else if (oleInit)
+	{
+		OleUninitialize();
+	}
 	return hr;
 }
